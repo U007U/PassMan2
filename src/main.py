72709bd463d993cb46fd23e266a5fb2cf3b271db
Dmitriy Yakushev <niceworks.youdo@gmail.com>
2024-09-20 18:01:42 +0500

    Добавлена модель для JWT токена

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ import uvicorn
 from worker.requestListener import app
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=80, log_level="info")
+    uvicorn.run(app, host="127.0.0.1", port=5555, log_level="info")
--- src/utils/models.py
+++ src/utils/models.py
@@ -1,7 +1,17 @@
+from pathlib import Path
+
 from pydantic import BaseModel
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class UserCredentials(BaseModel):
     service: str
     login: str
     password: str
+
+
+class Token(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "private-key.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "public-key.pem"
+    algorithm: str = "RS256"
